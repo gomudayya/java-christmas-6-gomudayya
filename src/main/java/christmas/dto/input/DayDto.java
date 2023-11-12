ee89fcdc59e93260f521c372946f15ea7266bf10
gomudayya <gomudayya@gmail.com>
2023-11-13 00:39:05 +0900

    feat : 날짜 검증 기능 구현

--- src/main/java/christmas/dto/input/DayDto.java
+++ src/main/java/christmas/dto/input/DayDto.java
@@ -1,10 +1,30 @@
 package christmas.dto.input;
 
+import validator.CommonValidator;
+
 public class DayDto {
     int dayOfMonth;
 
     public DayDto(String input) {
-        
+        validate(input);
+    }
+
+    private void validate(String input) {
+        try {
+            CommonValidator.validateIsInteger(input);
+
+            int day = Integer.parseInt(input);
+            validateDay(day);
+
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validateDay(int day) {
+        if (day < 1 || day > 31) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public int getDayOfMonth() {
--- src/main/java/validator/CommonValidator.java
+++ src/main/java/validator/CommonValidator.java
@@ -2,5 +2,11 @@ package validator;
 
 public class CommonValidator {
 
-
+    public static void validateIsInteger(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw e;
+        }
+    }
 }
